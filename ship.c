b9ef5c0f910934e29c19ba0c5708d73d8e55d842
Mike Changazi <changazi.mike@gmail.com>
2023-12-21 12:05:49 +0000

    added functions

--- ship.c
+++ ship.c
@@ -1,11 +1,34 @@
 #include<stdio.h>
 
+char direction[2];
+
 void go_south_east(int *lat, int *lon){
    *lat = *lat-1; //decrease the latitude
    *lon = *lon+1; // increase the logitude
 }
 
+void move_east(int *lon){
+   *lon = *lon +1;
+}
+
+void move_west(int *lon){
+     *lon = *lon-1;
+}
+
+void move_north_east(int *lat int *lon){
+    *lat = *lat-1;
+    *lon = *lon +1;
+}
+
+void move_north(int *lat){
+    *lat = *lat+1; }
+
 int main(){
+   
+   
+   printf("Enter direction: ");
+   scanf("%s", direction);
+   
    int latitude = 32;
    int longitude = -64;
 
