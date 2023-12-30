c8f69ac47c83bae55ad7250540508e2cd6c4232a
Mike Changazi <changazi.mike@gmail.com>
2023-12-30 09:04:00 +0000

    termux push

--- find.c
+++ find.c
@@ -32,20 +32,3 @@ int main(void){
 
  return 0;
 }
-
-
-
-
-
-
-
-
-void print_reverse(char *s)
-{
-size_t len = strlen(s);
-char *t = s+len - 1;
-while ( t>= s) {
-printf("%c", *t);
-t = t-1;
-}
-puts("");
